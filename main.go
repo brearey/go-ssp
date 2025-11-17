2a6e3923e789016379e1a57fbffc01caa3987917
Lorriant <brearey4@gmail.com>
2025-11-17 21:21:36 +0900

    player selecting element

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	ELEMENTS := map[int]string{
+		1: "Камень",
+		2: "Ножницы",
+		3: "Бумага",
+	}
+
 	fmt.Println("Добро пожаловать в игру Камень-ножницы-бумага. Автор Lorriant. Помощник Артур")
+
+	fmt.Println("Выберите ваш элемент. Нужно ввести номер.")
+	for key := 1; key < 4; key++ {
+		fmt.Printf("%d. %s\n", key, ELEMENTS[key])
+	}
+
+	var playerSelect int
+	for {
+		count, err := fmt.Scanf("%d", &playerSelect)
+		if (err != nil || count != 1 || playerSelect < 1 || playerSelect > 3) {
+			fmt.Fprint(os.Stderr, "Ты тупой? Нужно ввести число 1-3\n")
+			continue
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("Ваш выбор №: %s\n", ELEMENTS[playerSelect])
 }
